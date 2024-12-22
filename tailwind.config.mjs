bceb7e18931a40aa00d4a08b214c3257b4be5c29
shakibchakladar <shakibchakladar@gmail.com>
2024-12-23 00:10:19 +0600

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,5 +13,20 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF3811",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
