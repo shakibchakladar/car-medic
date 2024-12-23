5b207a0262525469af60b4dd34aaa110b6182f1b
shakibchakladar <shakibchakladar@gmail.com>
2024-12-23 15:38:08 +0600

    navbar added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,26 @@
+import Link from "next/link";
+
 const Navbar = () => {
+  const navItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Servicess",
+      path: "/servicess",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact Us",
+      path: "/contact",
+    },
+  ];
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 text-black container mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -42,33 +62,19 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link href="/">
+          <h2 className="font-extrabold text-xl shadow-sm rounded-md">
+            Car<span className="text-primary">Medic</span>
+          </h2>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+       {
+        navItems.map((item)=><Link className="px-5 font-semibold hover:text-primary duration-300" href={item.path} key={item.path}>{item.title}</Link>)
+       }
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn btn-outline btn-primary px-4">Button</a>
       </div>
     </div>
   );
